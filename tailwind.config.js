33500b08e9ffa2bf41fd4afdfd7e58893e7f79a0
Tony Ramírez <tonyramirez.business@outlook.com>
2022-09-27 21:55:22 -0500

    added tailwind colors and screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        limeBright: 'hsl(150, 88%, 59%)',
+        limeBrightLight: 'hsl(150, 88%, 69%)',
+        limeBrightSupLight: 'hsl(150, 88%, 95%)',
+        limePale: 'hsl(146, 100%, 96%)',
+        pinkDark: 'hsl(330, 39%, 23%)',
+        pinkDarkGrayish: 'hsl(329, 12%, 61%)',
+        pinkVeryDark: 'hsl(233, 12%, 13%)',
+        grayVeryLight: 'hsl(0, 0%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
